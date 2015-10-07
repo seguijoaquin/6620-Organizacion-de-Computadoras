dae2534179943eae2e48cb4ed15739af87971881
Alejandro Pernin <ale.pernin@gmail.com>
2015-10-07 14:54:20 -0300

    Creo carpeta de TP1

---
+++ tp1/src/pruebas.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+printf "Compilo\n"
+make
+
+printf "\n\n Las pruebas se pueden realizar manualmente mediante ./tp0.out < test_files/test#.txt\n"
+printf "\n\nCaso exitoso test1\n"
+./tp0.out < test_files/test1.txt
+
+printf "\n\nCaso exitoso test2\n"
+./tp0.out < test_files/test2.txt
+
+printf "\n\nCaso matrices incompatibles test3\n"
+./tp0.out < test_files/test3.txt
+
+printf "\n\nCaso cantidad impar de matrices test4\n"
+./tp0.out < test_files/test4.txt
+
+printf "\n\nCaso elementos < dimension definida test5\n"
+./tp0.out < test_files/test5.txt
+
+printf "\n\nCaso elementos > dimension definida test6\n"
+./tp0.out < test_files/test6.txt
